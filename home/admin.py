d0425297f65546375efd5cfd485ff77a91ada27f
KevinOh45 <kevinoh45@gmail.com>
2025-03-02 21:58:25 +0000

    Model: changed movie model to be more detailed and include reference to image. Chnaged admin section to include summernote functionality.

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Movie
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Movie)
+class MovieAdmin(SummernoteModelAdmin):
+    list_display = ('movie_title', 'slug', 'available')
+    search_fields = ['movie_title']
+    list_filter = ('available',)
+    prepopulated_fields = {'slug': ('movie_title',)}
+    summernote_fields = ('description',)
 
 # Register your models here.
-admin.site.register(Movie)
+#admin.site.register(Movie)
--- home/models.py
+++ home/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Free-Space"), (1, "Fully-Booked"))
 
 # Create your models here.
 class Movie(models.Model):
     movie_title = models.CharField(max_length=200, unique=True)
-    listing_author = models.ForeignKey(
+    slug = models.SlugField(max_length=200, unique=True, default="tempSlug")
+    author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="movie_title"
     )
     movie_desc = models.TextField()
+    movie_image = models.TextField(max_length=200, unique=True, default="tempImg")
+    movie_image_desc = models.TextField(max_length=200, unique=True, default="tempImgDesc")
     meal_name = models.CharField(max_length=200, unique=True)
     listed_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    available = models.IntegerField(choices=STATUS, default=0)
